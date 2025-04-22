ddb385f3afbc7d5dadcb76658811ce1786336a63
samuel-Dev06 <samuelmahembe3@gmail.com>
2025-04-22 11:46:31 +0200

    init commit

---
+++ src/test/java/zw/com/multipledatasource/MultipleDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.com.multipledatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
